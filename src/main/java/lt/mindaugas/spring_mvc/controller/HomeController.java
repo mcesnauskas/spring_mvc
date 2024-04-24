166b2a70296fa7738440f5fadc8df54e76ec5735
Mindaugas <mcesnauskas@gmail.com>
2024-04-24 14:49:03 +0300

    Initial commit

---
+++ src/main/java/lt/mindaugas/spring_mvc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package lt.mindaugas.spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+    @GetMapping("/home")
+    public String getHome(){
+        return "home";
+    }
+}
---
+++ src/test/java/lt/mindaugas/spring_mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mindaugas.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
