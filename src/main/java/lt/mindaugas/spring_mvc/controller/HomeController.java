626c7aa017d5fd6729dfe02f97f686017ad5d109
Mindaugas <mcesnauskas@gmail.com>
2024-04-24 15:47:59 +0300

    Add examples with Templates

--- src/main/java/lt/mindaugas/spring_mvc/controller/HomeController.java
+++ src/main/java/lt/mindaugas/spring_mvc/controller/HomeController.java
@@ -1,6 +1,8 @@
 package lt.mindaugas.spring_mvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class HomeController {
     @GetMapping("/home")
-    public String getHome(){
+    public String getHome(HttpServletRequest request, Model model){
+        String host = request.getServerName();
+        int port = request.getServerPort();
+        String baseUrl = "http://" + host + ":" + port;
+
+        model.addAttribute("attrName", "Welcome home!");
+        model.addAttribute("attrLink", baseUrl + "/test");
         return "home";
+    }
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "common/test";
     }
 }
---
+++ src/main/java/lt/mindaugas/spring_mvc/controller/TestController.java
@@ -0,0 +1,9 @@
+package lt.mindaugas.spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "")
+public class TestController {
+}
